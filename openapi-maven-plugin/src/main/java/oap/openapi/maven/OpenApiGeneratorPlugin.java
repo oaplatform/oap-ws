f80d4c62866f6e1c75a986ae13d6efb5a3acc0e5
alebastrov <alebastrov@mailinator.com>
2022-08-31 11:50:20 +0300

    added webservices visitor + refactored for using that visitor

--- openapi-maven-plugin/src/main/java/oap/openapi/maven/OpenApiGeneratorPlugin.java
+++ openapi-maven-plugin/src/main/java/oap/openapi/maven/OpenApiGeneratorPlugin.java
@@ -3,9 +3,8 @@ package oap.openapi.maven;
 import com.google.common.base.Joiner;
 import io.swagger.v3.core.util.Yaml;
 import oap.application.ApplicationException;
-import oap.application.module.Module;
 import oap.json.Binder;
-import oap.ws.WsConfig;
+import oap.ws.WebServicesWalker;
 import oap.ws.openapi.OpenapiGenerator;
 import oap.ws.openapi.OpenapiGeneratorSettings;
 import org.apache.commons.io.IOUtils;
@@ -18,16 +17,8 @@ import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.plugins.annotations.ResolutionScope;
 
-import org.codehaus.plexus.classworlds.realm.ClassRealm;
-
-import java.io.File;
 import java.io.FileOutputStream;
-import java.net.URL;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Objects;
 /**
@@ -52,70 +43,32 @@ public class OpenApiGeneratorPlugin extends AbstractMojo {
 
     @Parameter( required = true, readonly = true, defaultValue = "JSON" )
     private String outputType;
+
     @Override
     public void execute() throws MojoExecutionException, MojoFailureException {
+        Objects.requireNonNull( outputPath );
         getLog().info( "OpenAPI generation..." );
         try {
-            Objects.requireNonNull( outputPath );
-            LinkedHashSet<String> moduleConfigurations = new LinkedHashSet<>(  );
-
-            List<URL> urls = new ArrayList<>( Module.CONFIGURATION.urlsFromClassPath()
-                .stream()
-                .filter( url -> moduleConfigurations.add( url.toString() ) )
-                .toList() );
-            if ( classpath != null && !classpath.isEmpty() ) {
-                outputPath = classpath.get( 0 ) + "/swagger";
-                File file = new File( classpath.get( 0 ) + "/META-INF/oap-module.conf" );
-                if ( !Files.exists( Paths.get( file.getPath() ) ) ) {
-                    getLog().info( "File " + file.getPath() + " is missing, nothing to do" );
-                    return;
-                }
-                URL currentModuleUrl = file.toURI().toURL();
-                if ( moduleConfigurations.add( currentModuleUrl.toString() ) ) {
-                    urls.add( currentModuleUrl );
-                }
-            }
-            getLog().info( "Configurations (from oap-module.conf files) loaded: " + moduleConfigurations );
-
-            OpenapiGeneratorSettings settings = OpenapiGeneratorSettings
+            var settings = OpenapiGeneratorSettings
                 .builder()
                 .processOnlyAnnotatedMethods( false )
                 .outputType( OpenapiGeneratorSettings.Type.valueOf( outputType ) )
                 .build();
-            OpenapiGenerator openapiGenerator = new OpenapiGenerator( "title", "", settings );
-            LinkedHashSet<String> description = new LinkedHashSet<>();
+            var openapiGenerator = new OpenapiGenerator( "title", "", settings );
+            var visitor = new WebServiceVisitorForPlugin( pluginDescriptor, openapiGenerator, classpath, outputPath, getLog() );
 
-            urls.forEach( url -> {
-                getLog().info( "Reading config from " + url.getPath() );
-                Module config = Module.CONFIGURATION.fromUrl( url );
-                config.services.forEach( ( name, service ) -> {
-                    WsConfig wsService = ( WsConfig ) service.ext.get( "ws-service" );
-                    if ( wsService == null ) {
-                        getLog().debug( "Skipping bean: " + name );
-                        return;
-                    }
-                    getLog().debug( "WS bean: " + name + " implementing " + service.implementation );
-                    try {
-                        ClassRealm realm = pluginDescriptor.getClassRealm();
-                        Class clazz = realm.loadClass( service.implementation );
-                        OpenapiGenerator.Result result = openapiGenerator.processWebservice( clazz, wsService.path.stream().findFirst().orElse( "" ) );
-                        getLog().info( "WebService class " + clazz.getCanonicalName() + " " + result );
-                        description.add( clazz.getCanonicalName() );
-                    } catch( Exception e ) {
-                        getLog().warn( "Could not deal with module: " + name + " due to the implementation class '"
-                            + service.implementation + "' is unavailable", e );
-                    }
-                } );
-            } );
-            openapiGenerator.setDescription( "WS services: " + Joiner.on( ", " ).join( description ) );
+            WebServicesWalker.walk( visitor );
+            getLog().info( "Configurations (from oap-module.conf files) loaded: " + visitor.getModuleConfigurations() );
+
+            openapiGenerator.setDescription( "WS services: " + Joiner.on( ", " ).join( visitor.getDescription() ) );
             if ( settings.getOutputType() == OpenapiGeneratorSettings.Type.JSON ) {
                 String json = Binder.json.marshal( openapiGenerator.build() );
-                outputPath += ".json";
+                outputPath = visitor.getOutputPath() + ".json";
                 getLog().info( "OpenAPI JSON generated -> " + outputPath );
                 IOUtils.write( json.getBytes( StandardCharsets.UTF_8 ), new FileOutputStream( outputPath ) );
                 getLog().debug( "OpenAPI JSON is written to " + outputPath );
             } else {
-                outputPath += ".yml";
+                outputPath = visitor.getOutputPath() + ".yml";
                 String yaml = Yaml.mapper().writeValueAsString( openapiGenerator.build() );
                 getLog().info( "OpenAPI YAML generated -> " + outputPath );
                 IOUtils.write( yaml.getBytes( StandardCharsets.UTF_8 ), new FileOutputStream( outputPath ) );
