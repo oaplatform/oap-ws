f7fd04b2d94da229a634063fc8c1264bf84d2911
alebastrov <alebastrov@mailinator.com>
2022-12-19 13:47:46 +0200

    fix for endless loop in ApiWS (when we have some kind of Reference class in fields)

--- oap-ws-api-ws/src/test/java/oap/ws/api/ExampleWS.java
+++ oap-ws-api-ws/src/test/java/oap/ws/api/ExampleWS.java
@@ -41,7 +41,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
-import java.util.concurrent.atomic.AtomicReference;
 
 import static oap.http.server.nio.HttpServerExchange.HttpMethod.GET;
 import static oap.ws.WsParam.From.BODY;
